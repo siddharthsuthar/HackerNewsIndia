7d4d7f325d9d13b4500516a39b9a6058f67c3751
siddharth <siddharth.suthar053@gmail.com>
2019-01-26 15:20:01 -0800

    Imporved Boiler Plate

---
+++ hackernewsindia/src/components/HomePage.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+//This homepage will work as a Router for all the components and all the Paths so we determine what all paths we want and
+// route it from this file.
+
+class HomePage extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
